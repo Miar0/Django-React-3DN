0661c83ab8696683df9e551c3d0afa7da4f916f2
Danylo I <105504924+Miar0@users.noreply.github.com>
2025-04-11 17:13:45 +0300

    feat: add login and register modals

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,26 @@
-// src/App.jsx
 import Header from './components/Header'
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Modal from './components/Modal'
+import LoginForm from './components/LoginForm'
+import RegisterForm from './components/RegisterForm'
 
 function App() {
+    const [modalType, setModalType] = useState(null)
+
+    const closeModal = () => setModalType(null)
+
     return (
-        <>
-            <Header />
-        </>
+        <Router>
+            <Header
+                onLoginOpen={() => setModalType('login')}
+                onRegisterOpen={() => setModalType('register')}
+            />
+
+            <Modal isOpen={modalType !== null} onClose={closeModal}>
+                {modalType === 'login' ? <LoginForm /> : <RegisterForm />}
+            </Modal>
+        </Router>
     )
 }
 
