3fa27d12fb7fa43461d9a7f41c6d20dc3f16fa74
DenZastavnyi <denyszastavniy@gmail.com>
2025-04-13 14:05:35 +0300

    feat: add news-section and add more styles

---
+++ frontend/src/data/news.js
@@ -0,0 +1,29 @@
+import newsFirst from '../assets/images/news-home-first.png';
+import newsSecond from '../assets/images/news-home-second.png';
+import newsThird from '../assets/images/news-home-third.png';
+
+const news = [
+    {
+        id: 1,
+        image: newsFirst,
+        title: 'Понад дві тисячі врятованих котів: як хобі волонтерки стало організацією',
+        description: 'Історія волонтерки Яни Тябут з Черкас розпочалася 17 років тому — саме тоді вона забрала з вулиці свого першого кота Веніаміна.',
+        date: '10 April 19:50',
+    },
+    {
+        id: 2,
+        image: newsSecond,
+        title: '"Це Не Страшні Звірі, Вони Травмовані"',
+        description: 'До Дніпра евакуювали 21 цуценя та вагітну собаку з Донеччини. Історії тварин.',
+        date: '03 April 13:10',
+    },
+    {
+        id: 3,
+        image: newsThird,
+        title: 'У Львові загубився кіт, який жив у Ратуші.',
+        description: 'Кіт на імʼя Левчик загубився під час навчань у міській раді. Садовий обіцяє подарунок за розшук.',
+        date: '01 April 17:10',
+    },
+];
+
+export default news;
