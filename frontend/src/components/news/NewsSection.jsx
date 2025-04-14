b59c96110ebf53e435471dff14ce3e8474a2e43d
DenZastavnyi <denyszastavniy@gmail.com>
2025-04-14 20:45:37 +0300

    feat: add resposive for news section

--- frontend/src/components/news/NewsSection.jsx
+++ frontend/src/components/news/NewsSection.jsx
@@ -20,31 +20,34 @@ const NewsSection = () => {
             <div
                 className="max-w-[1220px] mx-auto flex flex-col lg:flex-row gap-4 bg-white dark:bg-white/5 backdrop-blur-xl rounded-3xl p-4 shadow-xl dark:shadow-none">
                 {/* Новини */}
-                <div className="w-[703px] flex flex-col gap-4">
+                <div className="w-full lg:w-[703px] flex flex-col gap-4">
                     {news.map((item) => (
                         <Link
                             to={`/news/${item.id}`}
                             key={item.id}
-                            className="group block w-[703px] h-[168px] relative rounded-xl overflow-hidden
-                            border border-[#3F3D9C]/10 dark:border-white/5 shadow-sm
-                            transition-all duration-300 ease-out
-                            hover:bg-[--color-news-hover-light] dark:hover:bg-white/10
-                            hover:shadow-xl hover:scale-[1.02] animate-fade-in-scale
-                            before:absolute before:left-0 before:top-4 before:bottom-4 before:w-1 before:rounded-xl
-                            before:bg-gradient-to-b before:from-[#9C9AFC] before:to-[#3F3D9C] before:opacity-0
-                            group-hover:before:opacity-100"
+                            className="group block w-full lg:w-[703px] h-auto lg:h-[168px] relative rounded-xl overflow-hidden
+            border border-[#3F3D9C]/10 dark:border-white/5 shadow-sm
+            transition-all duration-300 ease-out
+            hover:bg-[--color-news-hover-light] dark:hover:bg-white/10
+            hover:shadow-xl hover:scale-[1.02] animate-fade-in-scale
+            before:absolute before:left-0 before:top-4 before:bottom-4 before:w-1 before:rounded-xl
+            before:bg-gradient-to-b before:from-[#9C9AFC] before:to-[#3F3D9C] before:opacity-0
+            group-hover:before:opacity-100"
                         >
-                            <div className="flex gap-4 w-full h-full p-4">
-                                <div className="w-[168px] h-full overflow-hidden rounded-lg">
+                            <div
+                                className="flex flex-col lg:flex-row gap-4 w-full h-full p-4">
+                                <div
+                                    className="w-full lg:w-[168px] h-[200px] lg:h-full overflow-hidden rounded-lg">
                                     <img
                                         src={item.image}
                                         alt={item.title}
                                         className="w-full h-full object-cover transition-transform duration-500 ease-in-out
-                                        group-hover:scale-105 group-hover:shadow-md"
+                        group-hover:scale-105 group-hover:shadow-md"
                                     />
                                 </div>
 
-                                <div className="flex-1 flex flex-col justify-between overflow-hidden">
+                                <div
+                                    className="flex-1 flex flex-col justify-between overflow-hidden">
                                     <p className="text-xs text-gray-500 dark:text-gray-300">{item.date}</p>
                                     <h3 className="text-base font-semibold leading-snug text-[--color-news-accent] dark:text-white">
                                         {item.title}
@@ -58,14 +61,15 @@ const NewsSection = () => {
                     ))}
                 </div>
 
+
                 {/* Зображення справа */}
-                <div className="flex-1 flex justify-end items-start mr-2">
-                    <div className="w-full max-w-[444px] h-full overflow-hidden rounded-2xl group">
+                <div className="hidden lg:flex flex-1 justify-end items-start mr-2">
+                    <div
+                        className="w-full max-w-[444px] h-full overflow-hidden rounded-2xl group">
                         <img
                             src={petsImage}
                             alt="Pets"
-                            className="w-full h-full object-cover rounded-2xl shadow-xl
-                            transition-transform duration-500 ease-in-out group-hover:scale-105"
+                            className="w-full h-full object-cover rounded-2xl shadow-xl transition-transform duration-500 ease-in-out group-hover:scale-105"
                         />
                     </div>
                 </div>
@@ -78,7 +82,8 @@ const NewsSection = () => {
                     bg-white shadow-md transition hover:bg-[#ECEBFF] hover:scale-105 active:scale-95 focus-visible:ring-2
                     dark:border-white dark:text-white dark:bg-transparent dark:hover:bg-white/10 cursor-pointer">
                         Дізнатись більше
-                        <span className="transition-transform duration-300 group-hover:translate-x-1">➜</span>
+                        <span
+                            className="transition-transform duration-300 group-hover:translate-x-1">➜</span>
                     </button>
                 </Link>
             </div>
